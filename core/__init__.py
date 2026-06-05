bf3542fd89b38ac9e2518220df919dd507845844
marwanmonsif <115842165+marwanmonsif@users.noreply.github.com>
2026-06-05 15:03:14 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -39,8 +39,8 @@ def create_app():
 
     # Inject now into all templates
     from datetime import datetime as dt
-@app.context_processor
-    def inject_globals():
+    @app.context_processor
+      def inject_globals():
         return {'now': dt.utcnow()}
 
     @app.before_request
